fc6b42ba7bcc6facd1bb78efe83b96bd18f25b63
namtrinh <thebimhouse.info@gmail.com>
2026-09-20 19:48:41 +0700

    perf: expose lightweight work gateway

--- src/lib/tool-profile.ts
+++ src/lib/tool-profile.ts
@@ -4,7 +4,7 @@ import path from "path";
 export type ToolProfileName = "full" | "slim";
 
 export const LOCAL_TOOL_CATALOG = [
-  "job_list", "job_create", "job_update", "job_remove", "job_export", "job_import", "job_select", "job_status", "job_stop", "job_switch",
+  "job_list", "job_create", "job_update", "job_remove", "job_export", "job_import", "job_select", "job_status", "job_stop", "job_switch", "work_tool",
   "read_text_file", "write_file", "edit_file", "multi_edit", "apply_patch", "glob", "grep", "list_directory", "move_file",
   "run_command", "shell_status", "shell_reset", "start_process", "process_output", "node_repl", "ponytail_turn",
   "git_status", "git_diff", "git_add", "git_commit", "git_restore", "agent_status", "project_context",
@@ -43,6 +43,7 @@ export const SLIM_CHATGPT_TOOLS = new Set([
   "job_status",
   "job_stop",
   "job_switch",
+  "work_tool",
   "read_text_file",
   "write_file",
   "edit_file",
